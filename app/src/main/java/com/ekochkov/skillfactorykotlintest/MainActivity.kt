dbf08705d022ce74d2acb5dc9a6afb3259ee7e98
evgenykochkov <crabbs100@gmail.com>
2021-10-18 19:38:30 +0300

    повесил ClickListener на кнопки в главном экране

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/MainActivity.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/MainActivity.kt
@@ -2,11 +2,40 @@ package com.ekochkov.skillfactorykotlintest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.ekochkov.skillfactorykotlintest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         println("test git")
+
+        binding.btnMainMenu.setOnClickListener {
+            showToast("главное меню")
+        }
+
+        binding.btnFavorites.setOnClickListener {
+            showToast("избранное")
+        }
+
+        binding.btnLater.setOnClickListener {
+            showToast("посмотреть позже")
+        }
+
+        binding.btnCompilations.setOnClickListener {
+            showToast("подборки")
+        }
+
+        binding.btnOptions.setOnClickListener {
+            showToast("настройки")
+        }
+
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
