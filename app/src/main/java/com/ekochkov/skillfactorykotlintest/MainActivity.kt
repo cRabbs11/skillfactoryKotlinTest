c95eaa88626cbffc6a80596115402fedd3a6b393
evgenykochkov <crabbs100@gmail.com>
2021-10-26 21:04:57 +0300

    убрал обработку нажатия кнопок в MainActivity

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/MainActivity.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ekochkov.skillfactorykotlintest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.ekochkov.skillfactorykotlintest.databinding.ActivityMainBinding
 
@@ -12,26 +13,6 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         println("test git")
-
-        binding.btnMainMenu.setOnClickListener {
-            showToast("главное меню")
-        }
-
-        binding.btnFavorites.setOnClickListener {
-            showToast("избранное")
-        }
-
-        binding.btnLater.setOnClickListener {
-            showToast("посмотреть позже")
-        }
-
-        binding.btnCompilations.setOnClickListener {
-            showToast("подборки")
-        }
-
-        binding.btnOptions.setOnClickListener {
-            showToast("настройки")
-        }
 
         binding.topAppBar.setNavigationOnClickListener {
             showToast("onNavigationClick")
