40cc65d8e68b75cc69e2c3bff49024d0906c7a4b
evgenykochkov <crabbs100@gmail.com>
2021-12-08 20:45:28 +0300

    добавил импорт библиотеки с SearchView

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/HomeFragment.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.ekochkov.skillfactorykotlintest.databinding.FragmentHomeBinding
