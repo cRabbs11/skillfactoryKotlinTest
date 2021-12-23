bdfb284232928d6d80ec582390eb5f6eb30f3541
evgenykochkov <crabbs100@gmail.com>
2021-12-23 20:18:34 +0300

    добавил в CompilesFragment.kt анимацию появления

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/CompilesFragment.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/CompilesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ekochkov.skillfactorykotlintest.databinding.FragmentCompilesBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,13 +19,12 @@ private const val ARG_PARAM2 = "param2"
  */
 class CompilesFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
+    private lateinit var binding: FragmentCompilesBinding
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
@@ -32,7 +32,14 @@ class CompilesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compiles, container, false)
+        binding = FragmentCompilesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        AnimationHelper.performFragmentCircularRevealAnimation(view, requireActivity(), 2)
+
     }
 
     companion object {
