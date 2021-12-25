c512f50749a1573da862ee4e9ac662ef05d7a660
evgenykochkov <crabbs100@gmail.com>
2021-12-25 21:54:07 +0300

    убрал magic numbers, убрал пустые строки

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/AnimationHelper.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/AnimationHelper.kt
@@ -9,37 +9,28 @@ import kotlin.math.hypot
 import kotlin.math.roundToInt
 
 object AnimationHelper {
-
     private const val menuItems = 4
+    private const val menuItemCenterDivider = 2
+    private const val firstItemСoef = 1
 
     fun performFragmentCircularRevealAnimation(view: View, activity: Activity, position: Int) {
-
         Executors.newSingleThreadExecutor().execute {
-
             while (true) {
-
                 if (view.isAttachedToWindow) {
-
                     activity.runOnUiThread {
-
-                        val itemCenter = view.width / (menuItems * 2)
-                        val step = (itemCenter * 2) * (position - 1) + itemCenter
+                        val itemCenter = view.width / (menuItems * menuItemCenterDivider)
+                        val step = (itemCenter * menuItemCenterDivider) * (position - firstItemСoef) + itemCenter
 
                         val x: Int = step
                         val y: Int = view.y.roundToInt() + view.height
-
                         val startRadius = 0
                         val endRadius = hypot(view.width.toDouble(), view.height.toDouble())
 
                         ViewAnimationUtils.createCircularReveal(view, x, y, startRadius.toFloat(), endRadius.toFloat()).apply {
-
                             duration = 400
-
                             interpolator = AccelerateDecelerateInterpolator()
-
                             start()
                         }
-
                         view.visibility = View.VISIBLE
                     }
                     return@execute
@@ -47,5 +38,4 @@ object AnimationHelper {
             }
         }
     }
-
 }
