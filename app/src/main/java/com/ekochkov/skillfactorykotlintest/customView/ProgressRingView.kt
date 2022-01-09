8729f3046cfa746f4871e429b743bf6f9b6bf5a6
evgenykochkov <crabbs100@gmail.com>
2022-01-09 18:14:05 +0300

    поправил имя класса

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/customView/ProgressRingView.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/customView/ProgressRingView.kt
@@ -5,7 +5,7 @@ import android.util.AttributeSet
 import android.view.View
 import com.ekochkov.skillfactorykotlintest.R
 
-class ProgreesRingView @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null): View(context, attributeSet) {
+class ProgressRingView @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null): View(context, attributeSet) {
 
     private var progressValue: Int
     private var ringStrokeValue: Float
@@ -14,8 +14,8 @@ class ProgreesRingView @JvmOverloads constructor(context: Context, attributeSet:
         val attributes = context.theme.obtainStyledAttributes(attributeSet, R.styleable.FilmRatingView, 0, 0)
 
         try {
-            progressValue = attributes.getInt(R.styleable.ProgreesRingView_progressValue, 0)
-            ringStrokeValue = attributes.getFloat(R.styleable.ProgreesRingView_ringStrokeValue, 10f)
+            progressValue = attributes.getInt(R.styleable.ProgressRingView_progressValue, 0)
+            ringStrokeValue = attributes.getFloat(R.styleable.ProgressRingView_ringStrokeValue, 10f)
         } finally {
             attributes.recycle()
         }
