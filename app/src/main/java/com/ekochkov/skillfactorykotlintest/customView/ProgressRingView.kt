7afb543aba1aad17353850e16b62bef3169e6007
evgenykochkov <crabbs100@gmail.com>
2022-01-09 18:15:39 +0300

    добавил paint для отрисовки View

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/customView/ProgressRingView.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/customView/ProgressRingView.kt
@@ -1,6 +1,8 @@
 package com.ekochkov.skillfactorykotlintest.customView
 
 import android.content.Context
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
 import com.ekochkov.skillfactorykotlintest.R
@@ -10,6 +12,10 @@ class ProgressRingView @JvmOverloads constructor(context: Context, attributeSet:
     private var progressValue: Int
     private var ringStrokeValue: Float
 
+    private lateinit var ratingRingPaint : Paint
+    private lateinit var ratingTextPaint : Paint
+    private lateinit var backgroundPaint : Paint
+
     init {
         val attributes = context.theme.obtainStyledAttributes(attributeSet, R.styleable.FilmRatingView, 0, 0)
 
@@ -18,6 +24,29 @@ class ProgressRingView @JvmOverloads constructor(context: Context, attributeSet:
             ringStrokeValue = attributes.getFloat(R.styleable.ProgressRingView_ringStrokeValue, 10f)
         } finally {
             attributes.recycle()
+        }
+
+        initPaint()
+    }
+
+    private fun initPaint() {
+        ratingRingPaint = Paint().apply {
+            color = context.getColor(R.color.green_dark)
+            style = Paint.Style.STROKE
+            strokeWidth = ringStrokeValue
+            isAntiAlias = true
+        }
+
+        ratingTextPaint = Paint().apply {
+            color = context.getColor(R.color.green_dark)
+            style = Paint.Style.FILL_AND_STROKE
+            textSize = 60f
+            typeface = Typeface.DEFAULT_BOLD
+        }
+
+        backgroundPaint = Paint().apply {
+            color = context.getColor(R.color.gray)
+            style = Paint.Style.FILL_AND_STROKE
         }
     }
 }
