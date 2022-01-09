ab894c454ce8104b24a9aef03cee1d9e6e3f688a
evgenykochkov <crabbs100@gmail.com>
2022-01-09 18:12:50 +0300

    добавил параметры прогресса и толщины линии прогресса

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/customView/ProgreesRingView.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/customView/ProgreesRingView.kt
@@ -3,5 +3,21 @@ package com.ekochkov.skillfactorykotlintest.customView
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import com.ekochkov.skillfactorykotlintest.R
 
-class ProgreesRingView @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null): View(context, attributeSet)
+class ProgreesRingView @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null): View(context, attributeSet) {
+
+    private var progressValue: Int
+    private var ringStrokeValue: Float
+
+    init {
+        val attributes = context.theme.obtainStyledAttributes(attributeSet, R.styleable.FilmRatingView, 0, 0)
+
+        try {
+            progressValue = attributes.getInt(R.styleable.ProgreesRingView_progressValue, 0)
+            ringStrokeValue = attributes.getFloat(R.styleable.ProgreesRingView_ringStrokeValue, 10f)
+        } finally {
+            attributes.recycle()
+        }
+    }
+}
