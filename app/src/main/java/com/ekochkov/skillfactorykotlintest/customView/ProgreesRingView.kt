c635601d06d154a48fd0a42841d4f1114cc1fd4c
evgenykochkov <crabbs100@gmail.com>
2022-01-09 18:08:16 +0300

    сделал класс ProgreesRingView

---
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/customView/ProgreesRingView.kt
@@ -0,0 +1,7 @@
+package com.ekochkov.skillfactorykotlintest.customView
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ProgreesRingView @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null): View(context, attributeSet)
