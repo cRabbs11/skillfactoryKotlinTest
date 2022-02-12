d582effb63d15a42ec9ab751c82bff1ef411ea77
evgenykochkov <crabbs100@gmail.com>
2022-02-13 00:29:05 +0300

    сделал получение значения последнего видимомого элемента в списке фильмов

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/view/fragments/HomeFragment.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
 import com.ekochkov.skillfactorykotlintest.FilmListAdapter
@@ -103,11 +104,11 @@ class HomeFragment : Fragment() {
             adapter = filmAdapter
             addItemDecoration(parallaxPosterDecorator)
             addOnScrollListener(object : RecyclerView.OnScrollListener() {
-                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                    super.onScrolled(recyclerView, dx, dy)
-                    if (binding.recyclerView.childCount > 3) {
-                        val view = binding.recyclerView.getChildAt(2)
-                    }
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    val layoutManager = (binding.recyclerView.layoutManager as LinearLayoutManager)
+                    val lastVisiblePosition = layoutManager.findLastCompletelyVisibleItemPosition()
+                    viewModel.getLastVisibleFilmInList(lastVisiblePosition)
                 }
             })
         }
