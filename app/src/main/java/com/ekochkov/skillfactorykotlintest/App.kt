02979f43a924859418d2a131b8ec74655217af86
evgenykochkov <crabbs100@gmail.com>
2022-02-12 23:17:00 +0300

    добавил в Interactor serviceRetrofit для работы с tmdbAPI

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/App.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/App.kt
@@ -3,6 +3,13 @@ package com.ekochkov.skillfactorykotlintest
 import android.app.Application
 import com.ekochkov.skillfactorykotlintest.data.FilmRepository
 import com.ekochkov.skillfactorykotlintest.domain.Interactor
+import com.ekochkov.skillfactorykotlintest.utils.TmdbAPI
+import com.ekochkov.skillfactorykotlintest.utils.TmdbApiConstants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class App: Application() {
     lateinit var repo: FilmRepository
@@ -12,7 +19,25 @@ class App: Application() {
         super.onCreate()
         instance = this
         repo = FilmRepository()
-        interactor = Interactor(repo)
+
+        val okHttpClient = OkHttpClient.Builder()
+                .callTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .addInterceptor(HttpLoggingInterceptor().apply {
+                    if(BuildConfig.DEBUG) {
+                        level = HttpLoggingInterceptor.Level.BASIC
+                    }
+                })
+                .build()
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl(TmdbApiConstants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build()
+
+        val service = retrofit.create(TmdbAPI::class.java)
+        interactor = Interactor(repo, service)
     }
 
     companion object {
