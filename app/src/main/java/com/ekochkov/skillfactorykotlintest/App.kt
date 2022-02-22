11390494a5b57b509c42d19b0395dcb3939ceb42
evgenykochkov <crabbs100@gmail.com>
2022-02-22 23:46:48 +0300

    убрал неиспользуемые библиотеки

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/App.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/App.kt
@@ -1,17 +1,9 @@
 package com.ekochkov.skillfactorykotlintest
 
 import android.app.Application
-import com.ekochkov.skillfactorykotlintest.data.FilmRepository
 import com.ekochkov.skillfactorykotlintest.di.AppComponent
 import com.ekochkov.skillfactorykotlintest.di.DaggerAppComponent
-import com.ekochkov.skillfactorykotlintest.domain.Interactor
-import com.ekochkov.skillfactorykotlintest.utils.TmdbAPI
-import com.ekochkov.skillfactorykotlintest.utils.TmdbApiConstants
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
+
 
 class App: Application() {
     lateinit var dagger: AppComponent
