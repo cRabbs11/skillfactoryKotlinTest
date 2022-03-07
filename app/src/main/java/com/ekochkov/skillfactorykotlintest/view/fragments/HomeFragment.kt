f263f193e2fc0f19043dbcd15b3029df3accb068
evgenykochkov <crabbs100@gmail.com>
2022-03-07 22:34:10 +0300

    убрал обновление списка фильмов в HomeFragment при вызове onViewCreated

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/view/fragments/HomeFragment.kt
@@ -68,7 +68,6 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.refreshFilms()
         viewModel.filmListLiveData.observe(viewLifecycleOwner, {
             filmsDB = it
         })
