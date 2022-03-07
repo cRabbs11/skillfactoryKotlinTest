da47582d4920d97935870cac7f5c0584eea344c1
evgenykochkov <crabbs100@gmail.com>
2022-03-07 21:27:49 +0300

    добавил в интерактор методы для выбора категории фильмов

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/domain/Interactor.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/domain/Interactor.kt
@@ -1,6 +1,7 @@
 package com.ekochkov.skillfactorykotlintest.domain
 
 import com.ekochkov.skillfactorykotlintest.data.FilmRepository
+import com.ekochkov.skillfactorykotlintest.data.PreferenceProvider
 import com.ekochkov.skillfactorykotlintest.data.entity.PopularFilmsDataDTO
 import com.ekochkov.skillfactorykotlintest.utils.API
 import com.ekochkov.skillfactorykotlintest.utils.Converter
@@ -10,11 +11,11 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class Interactor(private val repository: FilmRepository, private val tmdbRetrofitService: TmdbAPI) {
+class Interactor(private val repository: FilmRepository, private val tmdbRetrofitService: TmdbAPI, private val preferenceProvider: PreferenceProvider) {
     fun getFilmsDB(): List<Film> = repository.filmList
 
     fun getFilmsFromTmdb(page: Int, callBack: HomeFragmentViewModel.ApiCallback) {
-        tmdbRetrofitService.getFilms(API.KEY, "ru-RU", page).enqueue(object: Callback<PopularFilmsDataDTO> {
+        tmdbRetrofitService.getFilms(preferenceProvider.getDefaultTypeCategory(), API.KEY, "ru-RU", page).enqueue(object: Callback<PopularFilmsDataDTO> {
             override fun onResponse(call: Call<PopularFilmsDataDTO>, response: Response<PopularFilmsDataDTO>) {
                 callBack.onSuccess(Converter.convertTmdbListToDTOList(response.body()?.tmdbFilms))
             }
@@ -25,5 +26,13 @@ class Interactor(private val repository: FilmRepository, private val tmdbRetrofi
 
         })
 
+    }
+
+    fun getDefaultTypeCategory(): String {
+        return preferenceProvider.getDefaultTypeCategory()
+    }
+
+    fun setDefaultTypeCategory(typeCategory: String) {
+        preferenceProvider.saveDefaultTypeCategory(typeCategory)
     }
 }
