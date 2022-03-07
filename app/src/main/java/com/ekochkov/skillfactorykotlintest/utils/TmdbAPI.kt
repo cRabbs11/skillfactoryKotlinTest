1b974f81fc4da418876d265aa26580ceddfbe4c4
evgenykochkov <crabbs100@gmail.com>
2022-03-07 21:32:27 +0300

    поправил запрос к TmdbAPI с учетов выбора категории фильмов

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/utils/TmdbAPI.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/utils/TmdbAPI.kt
@@ -3,13 +3,15 @@ package com.ekochkov.skillfactorykotlintest.utils
 import com.ekochkov.skillfactorykotlintest.data.entity.PopularFilmsDataDTO
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TmdbAPI {
-    @GET("3/movie/popular")
+    @GET("3/movie/{path}")
     fun getFilms(
+            @Path("path") movie_path: String,
             @Query("api_key") apiKey: String,
             @Query("language") language: String,
-            @Query("page") page: Int
+            @Query("page") page: Int,
     ): Call<PopularFilmsDataDTO>
 }
