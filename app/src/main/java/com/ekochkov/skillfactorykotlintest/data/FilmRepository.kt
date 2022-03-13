73a20f8e92667564de57256f1ed557073773b71b
evgenykochkov <crabbs100@gmail.com>
2022-03-13 11:49:02 +0300

    добавил в FilmRepository.kt методы для записи/получения данных из локальной БД c помощью DatabaseHelper

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/data/FilmRepository.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/data/FilmRepository.kt
@@ -1,11 +1,14 @@
 package com.ekochkov.skillfactorykotlintest.data
 
-import com.ekochkov.skillfactorykotlintest.R
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import com.ekochkov.skillfactorykotlintest.domain.Film
 
-class FilmRepository {
+class FilmRepository(dbHelper: DatabaseHelper) {
 
     val filmList = setFilms()
+    private val sqlDb: SQLiteDatabase = dbHelper.writableDatabase
 
     private fun setFilms(): ArrayList<Film> {
         val films = ArrayList<Film>()
@@ -66,5 +69,47 @@ class FilmRepository {
             )
         )
         return films
+    }
+
+    fun putFilmInDB(film: Film) {
+        val cv = ContentValues()
+        cv.put(DatabaseHelper.COLUMN_TITLE, film.title)
+        cv.put(DatabaseHelper.COLUMN_DESCR, film.descr)
+        cv.put(DatabaseHelper.COLUMN_POSTER, film.poster)
+        cv.put(DatabaseHelper.COLUMN_RATING, film.rating)
+
+        val isInFav = if (film.isInFav) { 1 } else { 0 }
+        cv.put(DatabaseHelper.COLUMN_IN_FAV, isInFav)
+
+        sqlDb.insert(DatabaseHelper.TABLE_NAME, null, cv)
+    }
+
+    fun getAllFilmsFromBD(): List<Film> {
+        val films = arrayListOf<Film>()
+
+        val cursor = sqlDb.rawQuery("SELECT * FROM ${DatabaseHelper.TABLE_NAME}", null)
+        cursor.use {
+            if (it.moveToFirst()) {
+                do {
+                    films.add(convertFillmfromCursor(it))
+                } while (it.moveToNext())
+            }
+        }
+
+        return films
+    }
+
+    private fun convertFillmfromCursor(cursor: Cursor): Film {
+        val title = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_TITLE))
+        val descr = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_DESCR))
+        val poster = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_POSTER))
+        val rating = cursor.getInt(cursor.getColumnIndex(DatabaseHelper.COLUMN_RATING))
+        val isInFav = cursor.getInt(cursor.getColumnIndex(DatabaseHelper.COLUMN_IN_FAV))==1
+
+        return Film(title = title,
+                poster = poster,
+                descr = descr,
+                isInFav = isInFav,
+                rating = rating)
     }
 }
