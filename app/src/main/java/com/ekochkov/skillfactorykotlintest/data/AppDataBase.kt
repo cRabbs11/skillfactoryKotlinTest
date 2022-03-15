3791cd3f138c5c1e9e6888cb16b1a7e33e6adc8b
evgenykochkov <crabbs100@gmail.com>
2022-03-15 19:22:12 +0300

    сделал Database Room класс

---
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/data/AppDataBase.kt
@@ -0,0 +1,11 @@
+package com.ekochkov.skillfactorykotlintest.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.ekochkov.skillfactorykotlintest.data.dao.FilmDao
+import com.ekochkov.skillfactorykotlintest.data.entity.Film
+
+@Database(entities = [Film::class], version = 1, exportSchema = true)
+abstract class AppDataBase: RoomDatabase() {
+    abstract fun filmDao(): FilmDao
+}
