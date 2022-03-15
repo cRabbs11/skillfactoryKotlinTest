b1bba6bdf7e9c6809f6e3e0c61f7b8a0b9a07a25
evgenykochkov <crabbs100@gmail.com>
2022-03-15 20:32:49 +0300

    сделал внедрение FilmDao через dagger

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/di/modules/DatabaseModule.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/di/modules/DatabaseModule.kt
@@ -1,8 +1,10 @@
 package com.ekochkov.skillfactorykotlintest.di.modules
 
 import android.content.Context
-import com.ekochkov.skillfactorykotlintest.data.DatabaseHelper
+import androidx.room.Room
+import com.ekochkov.skillfactorykotlintest.data.AppDataBase
 import com.ekochkov.skillfactorykotlintest.data.FilmRepository
+import com.ekochkov.skillfactorykotlintest.data.dao.FilmDao
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -12,9 +14,15 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideFilmRepository(databaseHelper: DatabaseHelper): FilmRepository = FilmRepository(databaseHelper)
+    fun provideFilmDao(context: Context): FilmDao {
+        return Room.databaseBuilder(
+            context,
+            AppDataBase::class.java,
+            AppDataBase.CASHED_FILMS_TABLE_NAME
+        ).build().filmDao()
+    }
 
     @Singleton
     @Provides
-    fun provideDatabaseHelper(context: Context): DatabaseHelper = DatabaseHelper(context)
+    fun provideFilmRepository(filmDao: FilmDao): FilmRepository = FilmRepository(filmDao)
 }
