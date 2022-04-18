3921dcf0c7b8de2eff12c7c7095e8db5790401da
evgenykochkov <crabbs100@gmail.com>
2022-04-18 20:27:14 +0300

    добавил библиотеку для связи RxJava и Retrofit

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/utils/TmdbAPI.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/utils/TmdbAPI.kt
@@ -1,6 +1,7 @@
 package com.ekochkov.skillfactorykotlintest.utils
 
 import com.ekochkov.skillfactorykotlintest.data.entity.PopularFilmsDataDTO
+import io.reactivex.rxjava3.core.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -14,4 +15,20 @@ interface TmdbAPI {
             @Query("language") language: String,
             @Query("page") page: Int,
     ): Call<PopularFilmsDataDTO>
+
+    @GET("3/search/movie")
+    fun searchFilms(
+            @Query("api_key") apiKey: String,
+            @Query("query") query: String,
+            @Query("language") language: String,
+            @Query("page") page: Int,
+    ): Call<PopularFilmsDataDTO>
+
+    @GET("3/search/movie")
+    fun searchFilmsByObservable(
+        @Query("api_key") apiKey: String,
+        @Query("query") query: String,
+        @Query("language") language: String,
+        @Query("page") page: Int,
+    ): Observable<PopularFilmsDataDTO>
 }
