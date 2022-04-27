ee95afac2b5952707fceb7c926f6d9be9115afcf
evgenykochkov <crabbs100@gmail.com>
2022-04-27 19:41:20 +0300

    поправил dagger для получения retrofit

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/App.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/App.kt
@@ -1,11 +1,11 @@
 package com.ekochkov.skillfactorykotlintest
 
 import android.app.Application
+import com.ekochkov.base_rest.DaggerRemoteComponent
 import com.ekochkov.skillfactorykotlintest.di.AppComponent
 import com.ekochkov.skillfactorykotlintest.di.DaggerAppComponent
 import com.ekochkov.skillfactorykotlintest.di.modules.DatabaseModule
 import com.ekochkov.skillfactorykotlintest.di.modules.DomainModule
-import com.ekochkov.skillfactorykotlintest.di.modules.RemoteModule
 
 
 class App: Application() {
@@ -14,10 +14,11 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        val remoteProvider = DaggerRemoteComponent.create()
         dagger = DaggerAppComponent.builder()
             .databaseModule(DatabaseModule())
             .domainModule(DomainModule(this))
-            .remoteModule(RemoteModule())
+            .remoteProvider(remoteProvider)
             .build()
     }
 
--- app/src/main/java/com/ekochkov/skillfactorykotlintest/di/AppComponent.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/di/AppComponent.kt
@@ -1,8 +1,8 @@
 package com.ekochkov.skillfactorykotlintest.di
 
+import com.ekochkov.base_rest.RemoteProvider
 import com.ekochkov.skillfactorykotlintest.di.modules.DatabaseModule
 import com.ekochkov.skillfactorykotlintest.di.modules.DomainModule
-import com.ekochkov.skillfactorykotlintest.di.modules.RemoteModule
 import com.ekochkov.skillfactorykotlintest.di.modules.TestModule
 import com.ekochkov.skillfactorykotlintest.viewmodel.FavoritesFragmentViewModel
 import com.ekochkov.skillfactorykotlintest.viewmodel.HomeFragmentViewModel
@@ -12,9 +12,9 @@ import javax.inject.Singleton
 
 @Singleton
 @Component(
+    dependencies = [RemoteProvider::class],
     modules = [
         DatabaseModule::class,
-        RemoteModule::class,
         DomainModule::class,
         TestModule::class
     ]
