7bce8fd257f41a54f28f6c570cc09989d3970673
evgenykochkov <crabbs100@gmail.com>
2022-06-03 21:46:03 +0300

    перенес создание канала для нотификаций в App

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/App.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/App.kt
@@ -1,11 +1,18 @@
 package com.ekochkov.skillfactorykotlintest
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.ekochkov.base_rest.DaggerRemoteComponent
 import com.ekochkov.skillfactorykotlintest.di.AppComponent
 import com.ekochkov.skillfactorykotlintest.di.DaggerAppComponent
 import com.ekochkov.skillfactorykotlintest.di.modules.DatabaseModule
 import com.ekochkov.skillfactorykotlintest.di.modules.DomainModule
+import com.ekochkov.skillfactorykotlintest.utils.NotificationConstants.NOTIFICATION_CHANNEL_DESCRIPTION
+import com.ekochkov.skillfactorykotlintest.utils.NotificationConstants.NOTIFICATION_CHANNEL_ID
+import com.ekochkov.skillfactorykotlintest.utils.NotificationConstants.NOTIFICATION_CHANNEL_NAME
 
 
 class App: Application() {
@@ -20,6 +27,18 @@ class App: Application() {
             .domainModule(DomainModule(this))
             .remoteProvider(remoteProvider)
             .build()
+
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.Q) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, importance)
+            channel.description = NOTIFICATION_CHANNEL_DESCRIPTION
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     companion object {
