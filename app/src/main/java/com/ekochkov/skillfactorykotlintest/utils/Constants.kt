524c552999787e9a412ab8e34d1df09509b77878
evgenykochkov <crabbs100@gmail.com>
2022-06-03 21:46:31 +0300

    добавил константы

--- app/src/main/java/com/ekochkov/skillfactorykotlintest/utils/Constants.kt
+++ app/src/main/java/com/ekochkov/skillfactorykotlintest/utils/Constants.kt
@@ -6,4 +6,7 @@ object Constants {
     const val BUNDLE_KEY_FILM = "film"
 
     const val BUNDLE_INTENT_OPEN_FILM_FRAGMENT = 1
+
+    const val IN_MILLIS_MINUTE = 60000L
+    const val IN_MILLIS_HOUR = 3600000L
 }
